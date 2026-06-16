a7b3454a8254d8f22fbcb50bb47d9c336fbd9217
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-06-16 13:45:00 +0100

    Add files via upload

---
+++ lines.py
@@ -0,0 +1,19 @@
+print("\033c\033[47;31m")
+print("give me a string to convert ...")
+a=input()
+print("give me a var name ...")
+b=input().strip()
+counter=0
+backs=False
+for aa in a:
+  if aa=="\\":
+      backs=True
+  else:
+      if(backs):
+          print(b+"["+str(counter)+"]='\\"+aa+"';",end="")
+          back=False
+      else:
+          print(b+"["+str(counter)+"]='"+aa+"';",end="")
+  counter=counter+1
+
+print(b+"["+str(counter)+"]='"+"\\0"+"';")
