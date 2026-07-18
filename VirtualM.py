6ca193fc45b2bda3c03c2906d4cf72a88bb229e5
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-07-18 09:06:59 +0100

    Add files via upload

--- VirtualM.py
+++ VirtualM.py
@@ -1,15 +1,31 @@
 import jpype
 import jpype.imports
+from tkinter import filedialog
 
 print("\033c\033[47;30m")
 print("give me a .class to open?")
-a = "Hello.class"
-a=input().strip()
+a=filedialog.askopenfile(title="give me the .video pack file ?",defaultextension="*.video")
+a=a.name
+
 if not jpype.isJVMStarted():
     jpype.startJVM(classpath=["."])
 a=a.replace(".class","")
+a=a.replace("\\","/")
+
+aa=0
+while(1):
+    f=a.find("/")
+    if f>-1:
+       a=a[f+1:]
+    else:
+       break  
+ 
+a=a.strip()
+print("##################################################")
+print(a)
+
 Hello = jpype.JClass(a)
 
 Hello.main([])
 
-jpype.shutdownJVM()#a=input().strip()
+jpype.shutdownJVM()
